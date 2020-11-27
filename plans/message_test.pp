b83f67f09f604ffa11049611ee8fefb05e582c57
andrew <andrew.jones@puppet.com>
2020-11-27 14:39:39 +0000

    update

--- plans/message_test.pp
+++ plans/message_test.pp
@@ -1,6 +1,7 @@
 plan oratune::message_test (
   TargetSpec $targets,
   Optional[String[1]] $example_nul = 'test',
+  $oramessage
 ) {
-  return run_task('oratune::dynamicmessage', $targets, 'example_nul' => $example_nul)
+  return run_task('oratune::dynamicmessage', $targets, 'example_nul' => $oramessage)
 }
