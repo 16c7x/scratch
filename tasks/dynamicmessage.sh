e4dc04170fa8b563a5f48cc679bc67e597fb6111
andrew <andrew.jones@puppet.com>
2020-11-27 14:30:40 +0000

    update

--- tasks/dynamicmessage.sh
+++ tasks/dynamicmessage.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 example_env=$PT_example_nul
-echo "Environment: $PT_example_nul"
+echo "Environment: $PT_example_nul" > /tmp/message
 echo "Stdin:" 
 cat -
--- tasks/staticmessage.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env bash
-echo "your message is static"
