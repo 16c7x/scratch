8f461e25abcd815ad092ffaeb483885323837fde
Andrew Jones <andrew.jones@puppet.com>
2023-01-27 13:20:55 +0000

    Update message_test.pp

--- plans/message_test.pp
+++ plans/message_test.pp
@@ -1,6 +1,7 @@
 plan oratune::message_test (
   TargetSpec $targets,
 ) {
-  $outside_apply = lookup('oramessage')
+  $outside_apply = $migration_server
+  #$outside_apply = lookup('oramessage')
   run_task('oratune::dynamicmessage', $targets, 'example_nul' => $outside_apply)
 }
