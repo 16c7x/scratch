c2b7d7227b89a123060c9e2b09dc656e262712fa
Andrew Jones <andrew.jones@puppet.com>
2023-02-15 18:59:17 +0000

    Update status.pp

--- plans/status.pp
+++ plans/status.pp
@@ -10,7 +10,7 @@
 plan oratune::status(
   TargetSpec $targets,
 ) {
-  #$outside_apply = File::read(‘/etc/hostname’)
+  $outside_apply = File::read(‘/etc/hostname’)
   #$outside_apply = lookup('oramessage')
   #run_task('oratune::dynamicmessage', $targets, 'example_nul' => $outside_apply)
 }
