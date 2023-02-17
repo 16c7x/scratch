cf8e0825709fad19e6a3f9f57d79a973774aa224
Andrew Jones <andrew.jones@puppet.com>
2023-02-17 12:00:58 +0000

    Update status.pp

--- plans/status.pp
+++ plans/status.pp
@@ -10,7 +10,7 @@
 plan oratune::status(
   TargetSpec $targets,
 ) {
-  #$message = "my message" #File::read(‘/etc/hostname’)
-  $message = system::env('hostname')
+  $message = "my message" #File::read(‘/etc/hostname’)
+  #$message = system::env('hostname')
   run_task('oratune::dynamicmessage', $targets, 'example_nul' => $message)
 }
