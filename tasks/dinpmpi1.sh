eca2cbbe4ad97f41ef22eb348801c8e6ed02285c
Andrew Jones <andrew.jones@puppet.com>
2023-06-27 12:16:31 +0100

    almost done

--- tasks/dinpmpi1.sh
+++ tasks/dinpmpi1.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-echo $PT_dbname > /tmp/file
+command='su- dinpmpi1 -c \"/opt/ATSmesddb2bld/bin/dbld_create_db.ksh -d '$PT_dbname' -t TBD_AUTO -C '$PT_codeset' -P '$PT_pagesize' -S '$PT_collation' -T '$PT_territory' -r DFT -a '\'$PT_comment\'\"
+echo $command > /tmp/file
