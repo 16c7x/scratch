75b867cffeaaa9c0ab5d32b9a2ca5f3bcb1a0563
Andrew Jones <andrew.jones@puppet.com>
2023-06-29 18:48:19 +0100

    some experimenting

---
+++ manifests/timestamps.pp
@@ -0,0 +1,10 @@
+# @summary A short summary of the purpose of this class
+#
+# A description of what this class does
+#
+# @example
+#   include oratune::timestamps
+class scratch::timestamps {
+  $new_current_time  = generate('/bin/date', '+%Y-%m-%d %H:%M:%S')
+  notice($new_current_time)
+}
--- tasks/dinpmpi1.sh
+++ tasks/dinpmpi1.sh
@@ -2,3 +2,5 @@
 
 command='su- dinpmpi1 -c \"/opt/ATSmesddb2bld/bin/dbld_create_db.ksh -d '$PT_dbname' -t TBD_AUTO -C '$PT_codeset' -P '$PT_pagesize' -S '$PT_collation' -T '$PT_territory' -r DFT -a '\'$PT_comment\'\"
 echo $command > /tmp/file
+
+return $command
