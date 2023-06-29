dfb5e9cbb36392557c99126c540533b8d0154f8f
Andrew Jones <andrew.jones@puppet.com>
2023-06-29 19:35:53 +0100

    some experimenting

--- manifests/timestamps.pp
+++ manifests/timestamps.pp
@@ -19,8 +19,8 @@ class scratch::timestamps {
 
   # Is the last run time less than the current time minus 7 days.
   if $lastrun_time_s > ($current_time_s - $interval_s) {
-    notice("Last run was within the last ${days} days")
+    notify { "Last run was within the last ${days} days": }
   } else {
-    notice("It has been more than ${days} days since the last run")
+    notify { "It has been more than ${days} days since the last run": }
   }
 }
