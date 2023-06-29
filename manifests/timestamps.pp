f2c398d34fa29b5f823dcbfa72c670a6ca63fd3b
Andrew Jones <andrew.jones@puppet.com>
2023-06-29 19:33:16 +0100

    some experimenting

--- manifests/timestamps.pp
+++ manifests/timestamps.pp
@@ -6,29 +6,21 @@
 #   include oratune::timestamps
 class scratch::timestamps {
   # Inputs
-  #$days          = 7
-  #$current_time  = '2023-06-28'
-  #$last_run_time = '2023-06-20 11:29:46'
-
-  $new_current_time  = generate('/bin/date', '+%Y-%m-%d %H:%M:%S').chomp
-
-  $current_t = Integer(Timestamp($new_current_time).strftime('%s'))
-
-  notify { 'timestamp message':
-    message => $current_t,
-  }
+  $days          = 7
+  $last_run_time = '2023-06-20 11:29:46'
+  $current_time  = generate('/bin/date', '+%Y-%m-%d %H:%M:%S').chomp
 
   # Convert the time stamps into seconds since Epoch.
-  #$current_t = Integer(Timestamp($current_time).strftime('%s'))
-  #$lastrun_t = Integer(Timestamp($last_run_time).strftime('%s'))
+  $current_time_s = Integer(Timestamp($current_time).strftime('%s'))
+  $lastrun_time_s = Integer(Timestamp($last_run_time).strftime('%s'))
 
   # Calculate the number of seconds in the time period we want.
-  #$interval = Integer(Timespan({ days => $days }).strftime('%S'))
+  $interval_s = Integer(Timespan({ days => $days }).strftime('%S'))
 
   # Is the last run time less than the current time minus 7 days.
-  #if $lastrun_t > ($current_t - $interval) {
-  #  notice("Last run was within the last ${days} days")
-  #} else {
-  #  notice("It has been more than ${days} days since the last run")
-  #}
+  if $lastrun_time_s > ($current_time_s - $interval_s) {
+    notice("Last run was within the last ${days} days")
+  } else {
+    notice("It has been more than ${days} days since the last run")
+  }
 }
